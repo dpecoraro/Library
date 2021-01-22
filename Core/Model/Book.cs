fa399276775c0cb38adf960523cac49f77030716
Demetrius Pecoraro <demetrius.pecoraro@totvs.com.br>
2021-01-21 21:15:13 -0300

    reference fix

---
+++ Core/Model/Book.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Model
+{
+    public class Book
+    {
+        public long Id { get; set; }
+
+        public double Price { get; set; }
+
+        public Costumer Costumer { get; set; }
+
+        public DateTime? SalesDate { get; set; }
+
+        public DateTime? EntryDate { get; set; }
+
+        private string _name;
+
+        private string _author;
+
+        private string _barCode;
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value?.Trim();
+        }
+
+        public string Author
+        {
+            get => _author;
+            set => _author = value?.Trim();
+        }
+
+        public string BarCode
+        {
+            get => _barCode;
+            set => _barCode = value?.Trim().ToUpper();
+        }
+       
+    }
+}
---
+++ Core/Model/Costumer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Core.Model
+{
+    public class Costumer
+    {
+        public long Id { get; set; }
+
+        [RegularExpression(@"^[a-zA-Z''-'\s]{1,40}$",
+        ErrorMessage = "Characters are not allowed.")]
+        public string Name { get; set; }
+
+        [RegularExpression(@"^\d{3}\.\d{3}\.\d{3}-\d{2}$",
+        ErrorMessage = "Check the Costumer CPF.")]
+        public string CPF { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public bool AllowStorageData { get; set; }
+
+        public IList<Book> Books { get; set; }
+    }
+}
